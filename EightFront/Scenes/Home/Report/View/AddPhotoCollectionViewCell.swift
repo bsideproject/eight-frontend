bad86128281de755a89ec4bafa248de8a781510c
wargi <wkahdla12346@gmail.com>
2022-09-16 00:12:06 +0900

    feature: 사진 추가 버튼 셀 추가

--- EightFront/Scenes/Home/Report/View/AddPhotoCollectionViewCell.swift
+++ EightFront/Scenes/Home/Report/View/AddPhotoCollectionViewCell.swift
@@ -5,4 +5,41 @@
 //  Created by wargi on 2022/10/10.
 //
 
-import Foundation
+import UIKit
+//MARK: Image 추가 cell
+final class AddPhotoCollectionViewCell: UICollectionViewCell {
+    //MARK: - Properties
+    // 사진추가 icon_camera => imageView
+    let imageView = UIImageView().then {
+        $0.image = Images.Report.addPhoto.image
+    }
+    
+    //MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        makeUI()
+    }
+    
+    //MARK: - Configure UI
+    private func makeUI() {
+        layer.cornerRadius = 4.0
+        layer.borderWidth = 1.0
+        layer.borderColor = Colors.gray006.color.cgColor
+        
+        contentView.backgroundColor = .white
+        contentView.addSubview(imageView)
+        
+        imageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(24)
+        }
+    }
+}
