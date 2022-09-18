05ddd913fdd92e5851067d04c9b39ed04b8fe563
wargi <wkahdla12346@gmail.com>
2022-09-18 18:11:22 +0900

    Etc: LogUtil 메서드 추가

--- EightFront/Utility/LogUtil.swift
+++ EightFront/Utility/LogUtil.swift
@@ -18,15 +18,100 @@ final class LogUtil {
         case s = "[🔥]" // severe
     }
     
+    // error
+    public class func e( _ object: Any,// 1
+        filename: String = #file, // 2
+        line: Int = #line, // 3
+        column: Int = #column, // 4
+        funcName: String = #function) {
+        print("""
+            ------------------------------------------------------------------------------------
+            \(Date().toString) \(LogEvent.e.rawValue)[\(sourceFileName(filePath: filename))]
+            Line:\(line)
+            Column:\(column)
+            Function:\(funcName) ->
+            \(object)
+            ------------------------------------------------------------------------------------
+            """)
+    }
+    
+    // info
+    public class func i( _ object: Any,// 1
+        filename: String = #file, // 2
+        line: Int = #line, // 3
+        column: Int = #column, // 4
+        funcName: String = #function) {
+        print("""
+            ------------------------------------------------------------------------------------
+            \(Date().toString) \(LogEvent.i.rawValue)[\(sourceFileName(filePath: filename))]
+            Line:\(line)
+            Column:\(column)
+            Function:\(funcName) ->
+            \(object)
+            ------------------------------------------------------------------------------------
+            """)
+    }
+    
     // debug
-    static func d( _ object: Any,
-        filename: String = #file,
-        line: Int = #line,
-        column: Int = #column,
+    public class func d( _ object: Any,// 1
+        filename: String = #file, // 2
+        line: Int = #line, // 3
+        column: Int = #column, // 4
         funcName: String = #function) {
         print("""
             ------------------------------------------------------------------------------------
             \(Date().toString) \(LogEvent.d.rawValue)[\(sourceFileName(filePath: filename))]
+            Line:\(line)
+            Column:\(column)
+            Function:\(funcName) ->
+            \(object)
+            ------------------------------------------------------------------------------------
+            """)
+    }
+    
+    // verbose
+    public class func v( _ object: Any,// 1
+        filename: String = #file, // 2
+        line: Int = #line, // 3
+        column: Int = #column, // 4
+        funcName: String = #function) {
+        print("""
+            ------------------------------------------------------------------------------------
+            \(Date().toString) \(LogEvent.v.rawValue)[\(sourceFileName(filePath: filename))]
+            Line:\(line)
+            Column:\(column)
+            Function:\(funcName) ->
+            \(object)
+            ------------------------------------------------------------------------------------
+            """)
+    }
+    
+    // warning
+    public class func w( _ object: Any,// 1
+        filename: String = #file, // 2
+        line: Int = #line, // 3
+        column: Int = #column, // 4
+        funcName: String = #function) {
+        print("""
+            ------------------------------------------------------------------------------------
+            \(Date().toString) \(LogEvent.w.rawValue)[\(sourceFileName(filePath: filename))]
+            Line:\(line)
+            Column:\(column)
+            Function:\(funcName) ->
+            \(object)
+            ------------------------------------------------------------------------------------
+            """)
+    }
+    
+    // severe
+    public class func s( _ object: Any,// 1
+        filename: String = #file, // 2
+        line: Int = #line, // 3
+        column: Int = #column, // 4
+        funcName: String = #function) {
+        print("""
+            ------------------------------------------------------------------------------------
+            \(Date().toString) \(LogEvent.s.rawValue)[\(sourceFileName(filePath: filename))]
             Line:\(line)
             Column:\(column)
             Function:\(funcName) ->
