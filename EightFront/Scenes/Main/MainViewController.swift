943adbd329138f8ecbb4a52f02f2ec673b7c5045
wargi <wkahdla12346@gmail.com>
2022-09-19 14:44:32 +0900

    Etc: 네이버맵 추가

--- EightFront/Scenes/Main/MainViewController.swift
+++ EightFront/Scenes/Main/MainViewController.swift
@@ -6,10 +6,31 @@
 //
 
 import UIKit
+import NMapsMap
+import SnapKit
+import Then
 
 final class MainViewController: UIViewController {
+    let mapView = NMFMapView().then {
+        $0.mapType = .navi
+        $0.positionMode = .direction
+        $0.minZoomLevel = 5.0
+        $0.maxZoomLevel = 18.0
+        $0.extent = NMGLatLngBounds(southWestLat: 31.43, southWestLng: 122.37, northEastLat: 44.35, northEastLng: 132)
+    }
+    
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        makeUI()
+    }
+    
+    func makeUI() {
+        view.addSubview(mapView)
+        
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
