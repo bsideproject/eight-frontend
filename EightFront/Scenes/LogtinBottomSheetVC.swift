e01d918817904a4dc862095d327e961165475087
Wan <82762650+krwd1117@users.noreply.github.com>
2022-10-01 17:44:03 +0900

    feature: 카카로 로그인 셋팅

--- EightFront/Scenes/LogtinBottomSheetVC.swift
+++ EightFront/Scenes/LogtinBottomSheetVC.swift
@@ -164,7 +164,7 @@ final class LogtinBottomSheetVC: UIViewController {
     }
     
     /// 애플 로그인
-    @objc private func appleLoginButtonTapped() {
+    private func appleLoginButtonTapped() {
         let request = ASAuthorizationAppleIDProvider().createRequest()
         request.requestedScopes = [.fullName, .email]
         
