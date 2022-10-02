2696a138c8786726a587c260eed018240fea32f4
wargi <wkahdla12346@gmail.com>
2022-10-03 03:23:08 +0900

    feature: Home에서 이동하는 VC 추가

--- EightFront/Scenes/Home/Search/HomeSearchVC.swift
+++ EightFront/Scenes/Home/Search/HomeSearchVC.swift
@@ -5,4 +5,30 @@
 //  Created by wargi on 2022/10/03.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+import Combine
+
+//MARK: HomeSearchVC
+final class HomeSearchVC: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        makeUI()
+        bind()
+    }
+    
+    //MARK: - Make UI
+    func makeUI() {
+        view.backgroundColor = .white
+    }
+    
+    //MARK: - Rx Binding..
+    func bind() {
+        
+    }
+}
