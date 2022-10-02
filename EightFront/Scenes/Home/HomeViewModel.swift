322a8addbf2b664ddb3aca84ed7aefb3676bb779
wargi <wkahdla12346@gmail.com>
2022-10-02 14:22:11 +0900

    feature: 네트워크 통신 코드 작성

--- EightFront/Scenes/Home/HomeViewModel.swift
+++ EightFront/Scenes/Home/HomeViewModel.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import Combine
+import Moya
+import CombineMoya
 
 //MARK: HomeViewModel
 final class HomeViewModel {
@@ -14,14 +16,15 @@ final class HomeViewModel {
     var cancelBag = Set<AnyCancellable>()
     let input = Input()
     let output = Output()
+    let clothesProvider = MoyaProvider<ClothesAPI>()
     
     //MARK: Initializer
     init() {
-        rxBind()
+        bind()
     }
     
     //MARK: RxBinding..
-    func rxBind() {
+    func bind() {
         
     }
 }
@@ -43,5 +46,19 @@ extension HomeViewModel {
 
 //MARK: - Method
 extension HomeViewModel {
-    
+    func requestCoordinates() {
+        clothesProvider.requestPublisher(.coordinates)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    LogUtil.d(error.localizedDescription)
+                case .finished:
+                    LogUtil.d("Successed")
+                }
+            } receiveValue: { response in
+                guard let responseData = try? response.map(CoordinatesResponse.self).data else { return }
+                LogUtil.d(responseData)
+            }
+            .store(in: &cancelBag)
+    }
 }
