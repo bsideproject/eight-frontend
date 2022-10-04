6e0d489dd2d448bfdfb2c14c4325623f47f661ad
wargi <wkahdla12346@gmail.com>
2022-10-05 00:30:16 +0900

    feature: BottomAddressView

--- EightFront/Scenes/Home/Report/View/BottomAddressView.swift
+++ EightFront/Scenes/Home/Report/View/BottomAddressView.swift
@@ -5,4 +5,34 @@
 //  Created by wargi on 2022/10/03.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+//MARK: BottomAddressView
+final class BottomAddressView: UIView {
+    //MARK: - Properties
+    let addressLabel = UILabel().then {
+        $0.textAlignment = .center
+    }
+    
+    //MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Not Created View")
+    }
+    
+    //MARK: - Make UI
+    func makeUI() {
+        addSubview(addressLabel)
+        
+        addressLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
