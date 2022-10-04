eb533d493e4610cbd5ce40468c30c27f32ffdc09
wargi <wkahdla12346@gmail.com>
2022-10-05 00:31:34 +0900

    feature: FindAddressViewModel 추가

--- EightFront/Scenes/Home/Report/FindAddressViewModel.swift
+++ EightFront/Scenes/Home/Report/FindAddressViewModel.swift
@@ -5,4 +5,54 @@
 //  Created by wargi on 2022/10/03.
 //
 
-import Foundation
+import UIKit
+import Combine
+import CoreLocation
+
+//MARK: FindAddressViewModel
+final class FindAddressViewModel {
+    //MARK: - Properties
+    var cancelBag = Set<AnyCancellable>()
+    let input = Input()
+    let output = Output()
+    @Published var addressString: String?
+    
+    //MARK: Initializer
+    init() {
+        bind()
+    }
+    
+    //MARK: RxBinding..
+    func bind() {
+        input.requestAddress
+            .sink {
+                if case let .failure(error) = $0 {
+                    LogUtil.d(error.localizedDescription)
+                }
+            } receiveValue: {
+                LocationManager.shared.addressUpdate(location: $0) { [weak self] address in
+                    self?.addressString = address
+                }
+            }
+            .store(in: &cancelBag)
+    }
+}
+
+//MARK: - I/O & Error
+extension FindAddressViewModel {
+    enum ErrorResult: Error {
+        case someError
+    }
+    
+    struct Input {
+        var requestAddress = CurrentValueSubject<CLLocation?, ErrorResult>.init(nil)
+    }
+    
+    struct Output {
+        
+    }
+}
+
+//MARK: - Method
+extension FindAddressViewModel {
+}
