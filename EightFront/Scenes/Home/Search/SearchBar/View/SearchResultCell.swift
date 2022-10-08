707d51fe3133ee6e48db41e205dbdf20d06e9475
wargi <wkahdla12346@gmail.com>
2022-10-09 02:01:08 +0900

    feature: POI 검색 결과 Cell

--- EightFront/Scenes/Home/Search/SearchBar/View/SearchResultCell.swift
+++ EightFront/Scenes/Home/Search/SearchBar/View/SearchResultCell.swift
@@ -5,4 +5,34 @@
 //  Created by wargi on 2022/10/07.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+//MARK: SearchResultCell
+final class SearchResultCell: UITableViewCell {
+    //MARK: - Properties
+    let keywordLabel = UILabel().then {
+        $0.font = Fonts.Pretendard.regular.font(size: 20)
+    }
+    
+    //MARK: - Initializer
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+        addSubview(keywordLabel)
+        
+        keywordLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
