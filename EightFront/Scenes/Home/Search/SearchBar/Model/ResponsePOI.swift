d65189e12d3d055707648c533dfabb412772fd1e
wargi <wkahdla12346@gmail.com>
2022-10-08 17:46:44 +0900

    feature: POI 데이터 모델링

--- EightFront/Scenes/Home/Search/SearchBar/Model/ResponsePOI.swift
+++ EightFront/Scenes/Home/Search/SearchBar/Model/ResponsePOI.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import CoreLocation
+
+struct ResponsePOI: Hashable {
+    var id = UUID()
+    let name: String?
+    let coordinate: CLLocationCoordinate2D?
+}
+
+extension ResponsePOI {
+    static func == (lhs: ResponsePOI, rhs: ResponsePOI) -> Bool {
+        return lhs.name == rhs.name &&
+                lhs.coordinate?.latitude == rhs.coordinate?.latitude &&
+                lhs.coordinate?.longitude == rhs.coordinate?.longitude &&
+                lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+        hasher.combine(coordinate?.latitude)
+        hasher.combine(coordinate?.longitude)
+    }
+}
