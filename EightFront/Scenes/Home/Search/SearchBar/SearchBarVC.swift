084b7f52cb3da65f2e886e57ce8f7167a6893fc2
wargi <wkahdla12346@gmail.com>
2022-10-09 02:02:03 +0900

    feature: POI 검색 VC 개발

--- EightFront/Scenes/Home/Search/SearchBar/SearchBarVC.swift
+++ EightFront/Scenes/Home/Search/SearchBar/SearchBarVC.swift
@@ -9,18 +9,29 @@ import Then
 import SnapKit
 import UIKit
 import Combine
+import CoreLocation
+
+protocol SearchBarDelegate: AnyObject {
+    func fetch(coordinate: CLLocationCoordinate2D?)
+}
 
 //MARK: SearchBarVC
 final class SearchBarVC: UIViewController {
     //MARK: - Properties
+    enum Section {
+        case search
+    }
+    weak var delegate: SearchBarDelegate?
+    private let viewModel = SearchBarViewModel()
     lazy var searchBar = UISearchBar().then {
-        $0.delegate = self
         self.navigationItem.titleView = $0
     }
     lazy var searchResultTableView = UITableView().then {
         $0.delegate = self
-        $0.dataSource = self
+        $0.keyboardDismissMode = .onDrag
+        SearchResultCell.register($0)
     }
+    var dataSource: UITableViewDiffableDataSource<Section, ResponsePOI>?
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
@@ -46,24 +57,40 @@ final class SearchBarVC: UIViewController {
                                                            target: self,
                                                            action: #selector(backButtonTouched))
         
-        
-        view.addSubview(searchBar)
         view.addSubview(searchResultTableView)
         
-        searchBar.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.left.right.equalToSuperview()
-            $0.height.equalTo(42)
-        }
         searchResultTableView.snp.makeConstraints {
-            $0.top.equalTo(searchBar.snp.bottom)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.left.right.bottom.equalToSuperview()
         }
+        
+        performDataSource()
     }
     
     //MARK: - Rx Binding..
     private func bind() {
+        searchBar
+            .textDidChangePublisher
+            .receive(on: DispatchQueue.global())
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .sink { [weak self] in
+                LogUtil.d($0)
+                self?.viewModel.input.requestPOI.send($0)
+            }
+            .store(in: &viewModel.bag)
         
+        viewModel
+            .output
+            .resultPOIs
+            .receive(on: DispatchQueue.main)
+            .sink {
+                if case let .failure(error) = $0 {
+                    LogUtil.d(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] in
+                self?.performDataSnapShot(pois: $0)
+            }
+            .store(in: &viewModel.bag)
     }
     
     @objc
@@ -72,16 +99,33 @@ final class SearchBarVC: UIViewController {
     }
 }
 
-extension SearchBarVC: UISearchBarDelegate {
-    
-}
-
-extension SearchBarVC: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+//MARK:- DiffDataSource
+extension SearchBarVC: UITableViewDelegate {
+    private func performDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, ResponsePOI>(tableView: searchResultTableView, cellProvider: { tableView, indexPath, poi in
+            let cell = tableView.dequeueReusableCell(withType: SearchResultCell.self, for: indexPath)
+            
+            cell.keywordLabel.text = poi.name
+            
+            return cell
+        })
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+    private func performDataSnapShot(pois: [ResponsePOI]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, ResponsePOI>()
+        snapshot.appendSections([.search])
+        
+        LogUtil.d(pois)
+        snapshot.appendItems(pois)
+        self.dataSource?.apply(snapshot, animatingDifferences: false)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.fetch(coordinate: viewModel.pois[indexPath.row].coordinate)
+        navigationController?.popViewController(animated: true)
     }
 }
