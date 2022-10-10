3520c7e5ffea8082e9325c453b4216922bcc9186
Wan <82762650+krwd1117@users.noreply.github.com>
2022-10-11 00:36:55 +0900

    Etc: LoginVC ViewController만 추가

---
+++ EightFront/Scenes/Auth/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  EightFront
+//
+//  Created by Jeongwan Kim on 2022/10/11.
+//
+
+import UIKit
+
+final class LoginVC: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeUI()
+    }
+    
+    // MARK: MakeUI
+    
+    private func makeUI() {
+        view.backgroundColor = .yellow
+    }
+    
+    
+    // MARK: Binding
+    
+}
