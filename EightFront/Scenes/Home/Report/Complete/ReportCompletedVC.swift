c3f9f8df59d039e9d162d936cc2382822e78bec1
wargi <wkahdla12346@gmail.com>
2022-10-12 10:43:23 +0900

    feature: 수정/삭제 완료 페이지

--- EightFront/Scenes/Home/Report/Complete/ReportCompletedVC.swift
+++ EightFront/Scenes/Home/Report/Complete/ReportCompletedVC.swift
@@ -5,4 +5,98 @@
 //  Created by wargi on 2022/10/10.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+import Combine
+
+//MARK: ReportCompletedVC
+final class ReportCompletedVC: UIViewController {
+    //MARK: - Properties
+    var bag = Set<AnyCancellable>()
+    let isInsert: Bool
+    let titleLabel = UILabel().then {
+        $0.textColor = Colors.gray001.color
+        $0.textAlignment = .center
+        $0.font = Fonts.Pretendard.regular.font(size: 24)
+    }
+    let subTitleLabel = UILabel().then {
+        $0.text = "N건 이상 접수된 건은 삭제 완료 처리됩니다."
+        $0.textColor = Colors.gray005.color
+        $0.textAlignment = .center
+        $0.font = Fonts.Pretendard.regular.font(size: 16)
+    }
+    let completedButton = UIButton().then {
+        $0.setImage(Images.Report.checkComplete.image)
+        $0.backgroundColor = Colors.gray001.color
+        $0.layer.cornerRadius = 39
+    }
+    let returnButton = UIButton().then {
+        $0.setTitle("돌아가기")
+        $0.setTitleColor(Colors.point.color)
+        $0.titleLabel?.font = Fonts.Pretendard.regular.font(size: 16)
+        $0.layer.backgroundColor = Colors.gray002.color.cgColor
+        $0.layer.cornerRadius = 4
+    }
+    
+    //MARK: - Life Cycle
+    init(isInsert: Bool) {
+        self.isInsert = isInsert
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        makeUI()
+        bind()
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        view.addSubview(subTitleLabel)
+        view.addSubview(returnButton)
+        view.addSubview(completedButton)
+        
+        titleLabel.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        subTitleLabel.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+        }
+        completedButton.snp.makeConstraints {
+            $0.size.equalTo(78)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(titleLabel.snp.top).offset(-24)
+        }
+        returnButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().offset(-24)
+            $0.height.equalTo(58)
+        }
+        
+        titleLabel.text = isInsert ? "수정 요청이 접수됐어요!" : "삭제 요청이 접수됐어요!"
+        subTitleLabel.isHidden = isInsert
+    }
+    
+    //MARK: - Rx Binding..
+    private func bind() {
+        returnButton
+            .tapPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in
+                self?.dismiss(animated: true)
+            }
+            .store(in: &bag)
+    }
+}
