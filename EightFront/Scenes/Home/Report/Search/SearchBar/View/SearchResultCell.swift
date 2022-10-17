992a46884db8b083044c4ef6a92b4dca9f8374cf
wargi <wkahdla12346@gmail.com>
2022-10-17 23:30:53 +0900

    feature: Report Searchbar UI 수정

--- EightFront/Scenes/Home/Report/Search/SearchBar/View/SearchResultCell.swift
+++ EightFront/Scenes/Home/Report/Search/SearchBar/View/SearchResultCell.swift
@@ -12,18 +12,16 @@ import UIKit
 final class SearchResultCell: UITableViewCell {
     //MARK: - Properties
     let titleLabel = UILabel().then {
-        $0.textColor = Colors.gray001.color
-        $0.font = Fonts.Templates.body1.font
-    }
-    let addressTitleLabel = UILabel().then {
-        $0.text = "주소"
-        $0.textColor = Colors.gray001.color
-        $0.textAlignment = .center
-        $0.backgroundColor = Colors.gray008.color
-        $0.font = Fonts.Templates.body1.font
+        $0.textColor = Colors.gray006.color
+        $0.font = Fonts.Templates.subheader3.font
     }
     let subTitleLabel = UILabel().then {
         $0.textColor = Colors.gray005.color
+        $0.font = Fonts.Templates.body1.font
+    }
+    let distanceLabel = UILabel().then {
+        $0.textColor = Colors.gray001.color
+        $0.textAlignment = .right
         $0.font = Fonts.Templates.body1.font
     }
     let lineView = UIView().then {
@@ -44,23 +42,22 @@ final class SearchResultCell: UITableViewCell {
     //MARK: - Make UI
     private func makeUI() {
         contentView.addSubview(titleLabel)
-        contentView.addSubview(addressTitleLabel)
         contentView.addSubview(subTitleLabel)
+        contentView.addSubview(distanceLabel)
         contentView.addSubview(lineView)
         
         titleLabel.snp.makeConstraints {
-            $0.top.left.right.equalTo(16)
-            $0.height.equalTo(22)
-        }
-        addressTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
-            $0.left.equalToSuperview().offset(16)
-            $0.width.equalTo(29)
+            $0.top.left.right.equalToSuperview().inset(16)
             $0.height.equalTo(26)
         }
+        distanceLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.right.equalToSuperview().offset(-16)
+        }
         subTitleLabel.snp.makeConstraints {
-            $0.left.equalTo(addressTitleLabel.snp.right).offset(8)
-            $0.centerY.equalTo(addressTitleLabel.snp.centerY)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.left.equalToSuperview().inset(16)
+            $0.right.equalTo(distanceLabel.snp.left).offset(4)
         }
         lineView.snp.makeConstraints {
             $0.left.right.equalToSuperview().inset(16)
