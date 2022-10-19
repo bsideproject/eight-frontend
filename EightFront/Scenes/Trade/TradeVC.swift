a52a52cac49dea5ab0faee77d683b46602a92b7d
wargi <wkahdla12346@gmail.com>
2022-10-19 10:01:08 +0900

    feature: 버릴까 말까 더미데이터 및 VC/VM 추가

--- EightFront/Scenes/Trade/TradeVC.swift
+++ EightFront/Scenes/Trade/TradeVC.swift
@@ -15,21 +15,67 @@ final class TradeVC: UIViewController {
         return .darkContent
     }
     
+    private var viewModel = TradeViewModel()
+    var stackContainer: StackContainerView!
+    
+    
     //MARK: - Life Cycle
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor(red:0.93, green:0.93, blue:0.93, alpha:1.0)
+        stackContainer = StackContainerView()
+        view.addSubview(stackContainer)
+        makeUI()
+        stackContainer.translatesAutoresizingMaskIntoConstraints = false
+        configureNavigationBarButtonItem()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        makeUI()
         bind()
     }
     
     //MARK: - Make UI
     private func makeUI() {
         view.backgroundColor = .white
+        
+        stackContainer.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-60)
+            $0.width.equalTo(300)
+            $0.height.equalTo(400)
+        }
     }
     
     //MARK: - Binding..
     private func bind() {
-        
+        stackContainer.dataSource = self
     }
+    
+    func configureNavigationBarButtonItem() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetTapped))
+    }
+    
+    //MARK: - Handlers
+    @objc
+    func resetTapped() {
+        stackContainer.reloadData()
+    }
+}
+
+extension TradeVC: SwipeCardsDataSource {
+    func numberOfCardsToShow() -> Int {
+        return viewModel.dummyData.count
+    }
+
+    func card(at index: Int) -> SwipeCardView {
+        let card = SwipeCardView()
+        card.dataSource = viewModel.dummyData[index]
+        return card
+    }
+
+    func emptyView() -> UIView? {
+        return nil
+    }   
 }
--- EightFront/Scenes/Trade/TradeViewModel.swift
+++ EightFront/Scenes/Trade/TradeViewModel.swift
@@ -5,4 +5,49 @@
 //  Created by wargi on 2022/10/26.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+//MARK: TradeViewModel
+final class TradeViewModel {
+    //MARK: - Properties
+    var bag = Set<AnyCancellable>()
+    let input = Input()
+    let output = Output()
+    var dummyData = [CardsDataModel(bgColor: UIColor(red:0.96, green:0.81, blue:0.46, alpha:1.0), name: "Hamburger", image: Images.Test.hamburger.image),
+                     CardsDataModel(bgColor: UIColor(red:0.29, green:0.64, blue:0.96, alpha:1.0), name: "Puppy", image: Images.Test.puppy.image),
+                     CardsDataModel(bgColor: UIColor(red:0.29, green:0.63, blue:0.49, alpha:1.0), name: "Poop", image: Images.Test.poop.image),
+                     CardsDataModel(bgColor: UIColor(red:0.69, green:0.52, blue:0.38, alpha:1.0), name: "Panda", image: Images.Test.panda.image),
+                     CardsDataModel(bgColor: UIColor(red:0.90, green:0.99, blue:0.97, alpha:1.0), name: "Subway", image: Images.Test.subway.image),
+                     CardsDataModel(bgColor: UIColor(red:0.83, green:0.82, blue:0.69, alpha:1.0), name: "Robot", image: Images.Test.robot.image)]
+    
+    //MARK: Initializer
+    init() {
+        bind()
+    }
+    
+    //MARK: RxBinding..
+    private func bind() {
+        
+    }
+}
+
+//MARK: - I/O & Error
+extension TradeViewModel {
+    enum ErrorResult: Error {
+        case someError
+    }
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+}
+
+//MARK: - Method
+extension TradeViewModel {
+    
+}
