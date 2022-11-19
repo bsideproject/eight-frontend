8c1e775e0dca0d9b186664006c49ad467bb5cd72
wargi <wkahdla12346@gmail.com>
2022-11-19 19:05:10 +0900

    feature: 디테일 화면 게시물/댓글 시간 표시

--- EightFront/Utility/DateManager.swift
+++ EightFront/Utility/DateManager.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+class DateManager {
+    private let formatter = DateFormatter().then {
+        $0.locale = Locale(identifier: "ko_kr")
+        $0.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS"
+    }
+    
+    static let shared = DateManager()
+    
+    private init() {}
+    
+    func timeString(target: String?) -> String? {
+        guard let date = formatter.date(from: target ?? "") else { return nil }
+        
+        let from = date.timeIntervalSince1970
+        let to = Date().timeIntervalSince1970
+        
+        let duration = to - from
+        
+        let minute = 60.0
+        let hour = minute * 60
+        let day = hour * 24
+        let month = day * 30
+        let year = day * 365
+        
+        if duration > year {
+            return "\(Int(duration / year))년 전"
+        } else if duration > month && duration < year {
+            return "\(Int(duration / month))달 전"
+        } else if duration > (day * 3) && duration < month {
+            return "\(Int(duration / year))일 전"
+        } else if duration > (day * 2) && duration < (day * 3) {
+            return "그저께"
+        } else if duration > day && duration < (day * 2) {
+            return "어제"
+        } else if duration > hour && duration < day {
+            return "\(Int(duration / hour))시간 전"
+        } else if duration > minute && duration < hour {
+            return "\(Int(duration / minute))분 전"
+        } else {
+            return "지금"
+        }
+    }
+}
