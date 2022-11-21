d4b58499f73ccd9e0e9b2d409597c10e3d72776a
wargi <wkahdla12346@gmail.com>
2022-11-21 22:07:52 +0900

    feature: 신고하기 구현

--- EightFront/Scenes/Clothes/Detail/Report/ReportPostVC.swift
+++ EightFront/Scenes/Clothes/Detail/Report/ReportPostVC.swift
@@ -15,8 +15,26 @@ import CombineCocoa
 final class ReportPostVC: UIViewController {
     //MARK: - Properties
     var bag = Set<AnyCancellable>()
+    private let viewModel = ReportPostViewModel()
     private let navigationView = CommonNavigationView().then {
         $0.titleLabel.text = "신고하기"
+    }
+    private let titleLabel = UILabel().then {
+        $0.text = "신고 사유"
+        $0.font = Fonts.Pretendard.semiBold.font(size: 16)
+    }
+    private lazy var tableView = UITableView().then {
+        $0.delegate = self
+        $0.dataSource = self
+        $0.separatorStyle = .none
+        ReportPostCell.register($0)
+    }
+    private let sumitButton = UIButton().then {
+        $0.setTitle("신고하기")
+        $0.isEnabled = false
+        $0.setTitleColor(.white)
+        $0.backgroundColor = Colors.gray006.color
+        $0.layer.cornerRadius = 8.0
     }
     
     //MARK: - Life Cycle
@@ -27,14 +45,42 @@ final class ReportPostVC: UIViewController {
         bind()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+        tabBarController?.tabBar.isHidden = true
+    }
+    
     //MARK: - Make UI
     private func makeUI() {
+        view.backgroundColor = .white
+        
         view.addSubview(navigationView)
+        view.addSubview(titleLabel)
+        view.addSubview(tableView)
+        view.addSubview(sumitButton)
         
         navigationView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.left.right.equalToSuperview()
             $0.height.equalTo(47)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(24)
+            $0.left.right.equalToSuperview().inset(16)
+            $0.height.equalTo(26)
+        }
+        sumitButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(16)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-24)
+            $0.height.equalTo(58)
+        }
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.left.right.equalToSuperview()
+            $0.bottom.equalTo(sumitButton.snp.top).offset(-16)
         }
     }
     
@@ -47,5 +93,47 @@ final class ReportPostVC: UIViewController {
                 self?.navigationController?.popViewController(animated: true)
             }
             .store(in: &bag)
+        
+        sumitButton
+            .tapPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                let completedVC = ReportCompletedVC(type: .report)
+                self?.navigationController?.pushViewController(completedVC, animated: true)
+            }
+            .store(in: &viewModel.bag)
+    }
+}
+
+extension ReportPostVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.reports.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withType: ReportPostCell.self, for: indexPath)
+        
+        cell.configure(with: viewModel.reports[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        
+        cell.isSelected = true
+        sumitButton.isEnabled = true
+        sumitButton.setTitleColor(Colors.point.color)
+        sumitButton.backgroundColor = Colors.gray002.color
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        
+        cell.isSelected = false
     }
 }
--- EightFront/Scenes/Clothes/Detail/Report/ReportPostViewModel.swift
+++ EightFront/Scenes/Clothes/Detail/Report/ReportPostViewModel.swift
@@ -5,4 +5,51 @@
 //  Created by wargi on 2022/11/20.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+//MARK: ReportPostViewModel
+final class ReportPostViewModel {
+    //MARK: - Properties
+    var bag = Set<AnyCancellable>()
+    let input = Input()
+    let output = Output()
+    let reports = [
+    "음란물",
+    "불법정보를 포함",
+    "청소년에게 유해한 내용",
+    "욕설/생명경시/혐오/차별적 표현",
+    "개인정보 노출 게시물",
+    "불쾌한 표현"
+    ]
+    
+    //MARK: Initializer
+    init() {
+        bind()
+    }
+    
+    //MARK: RxBinding..
+    private func bind() {
+        
+    }
+}
+
+//MARK: - I/O & Error
+extension ReportPostViewModel {
+    enum ErrorResult: Error {
+        case someError
+    }
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+}
+
+//MARK: - Method
+extension ReportPostViewModel {
+    
+}
