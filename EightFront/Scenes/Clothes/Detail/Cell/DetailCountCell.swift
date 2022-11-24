f6043e71570c982eceac3c8a194b3089be0b4ae2
wargi <wkahdla12346@gmail.com>
2022-11-24 20:50:09 +0900

    feature: 아이템 상세정보 셀 구현

--- EightFront/Scenes/Clothes/Detail/Cell/DetailCountCell.swift
+++ EightFront/Scenes/Clothes/Detail/Cell/DetailCountCell.swift
@@ -5,4 +5,79 @@
 //  Created by wargi on 2022/11/18.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+//MARK: 보관 및 버림 카운트 셀
+final class DetailCountCell: DetailPostCell {
+    //MARK: - Properties
+    let lockTitleLabel = UILabel().then {
+        $0.text = "보관해요"
+        $0.textColor = UIColor(colorSet: 117)
+        $0.font = Fonts.Templates.caption1.font
+    }
+    let lockCountLabel = UILabel().then {
+        $0.font = Fonts.Templates.caption1.font
+    }
+    let throwTitleLabel = UILabel().then {
+        $0.text = "버릴래요"
+        $0.textColor = UIColor(colorSet: 117)
+        $0.font = Fonts.Templates.caption1.font
+    }
+    let throwCountLabel = UILabel().then {
+        $0.font = Fonts.Templates.caption1.font
+    }
+    
+    //MARK: - Initializer
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(withLockCount lockCount: Int?, throwCount: Int?) {
+        lockCountLabel.text = "\(lockCount ?? 0)"
+        throwCountLabel.text = "\(throwCount ?? 0)"
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+        selectionStyle = .none
+        
+        contentView.addSubview(lockTitleLabel)
+        contentView.addSubview(lockCountLabel)
+        contentView.addSubview(throwTitleLabel)
+        contentView.addSubview(throwCountLabel)
+        
+        lockTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(23)
+            $0.left.equalToSuperview().offset(16)
+            $0.width.equalTo(42)
+            $0.height.equalTo(26)
+        }
+        lockCountLabel.snp.makeConstraints {
+            $0.centerY.equalTo(lockTitleLabel.snp.centerY)
+            $0.left.equalTo(lockTitleLabel.snp.right).offset(4)
+            $0.height.equalTo(26)
+        }
+        throwTitleLabel.snp.makeConstraints {
+            $0.centerY.equalTo(lockCountLabel.snp.centerY)
+            $0.left.equalTo(lockCountLabel.snp.right).offset(8)
+            $0.width.equalTo(42)
+            $0.height.equalTo(26)
+        }
+        throwCountLabel.snp.makeConstraints {
+            $0.centerY.equalTo(lockTitleLabel.snp.centerY)
+            $0.left.equalTo(throwTitleLabel.snp.right).offset(4)
+            $0.height.equalTo(26)
+        }
+    }
+}
+
+extension DetailCountCell {
+    static var height: CGFloat = 49.0
+}
--- EightFront/Scenes/Clothes/Detail/Cell/DetailDescriptionCell.swift
+++ EightFront/Scenes/Clothes/Detail/Cell/DetailDescriptionCell.swift
@@ -5,4 +5,61 @@
 //  Created by wargi on 2022/11/18.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+//MARK: 옷에 대한 설명 셀
+final class DetailDescriptionCell: DetailPostCell {
+    //MARK: - Properties
+    let titleLabel = UILabel().then {
+        $0.font = Fonts.Pretendard.semiBold.font(size: 20)
+    }
+    let descLabel = UILabel().then {
+        $0.numberOfLines = 0
+        $0.font = Fonts.Templates.subheader.font
+    }
+    
+    //MARK: - Initializer
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        makeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(withTitle title: String?, desc: String?) {
+        titleLabel.text = title
+        descLabel.text = desc
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+        selectionStyle = .none
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(22)
+            $0.left.right.equalToSuperview().inset(16)
+            $0.height.equalTo(26)
+        }
+        descLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(5)
+            $0.left.right.equalToSuperview().inset(16)
+        }
+    }
+}
+
+extension DetailDescriptionCell {
+    static func height(with desc: String?) -> CGFloat {
+        guard let desc else { return .zero }
+        
+        return 53.0 + UILabel.textHeight(withWidth: UIScreen.main.bounds.width - 32.0,
+                                         font: Fonts.Templates.subheader.font,
+                                         text: desc)
+    }
+}
