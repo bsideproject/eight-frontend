307e4fc7c2b0e8b7b32625f8b76e7c15fa515e51
wargi <wkahdla12346@gmail.com>
2022-11-24 22:41:35 +0900

    feature: 버릴까말까 상세페이지 구현

--- EightFront/Scenes/Clothes/Detail/Cell/ProfileCell.swift
+++ EightFront/Scenes/Clothes/Detail/Cell/ProfileCell.swift
@@ -5,4 +5,93 @@
 //  Created by wargi on 2022/11/13.
 //
 
-import Foundation
+import Then
+import SnapKit
+import UIKit
+
+protocol DetailPostProtocol {}
+typealias DetailPostCell = UITableViewCell & DetailPostProtocol
+
+//MARK: 최상단 프로파일 셀
+final class ProfileCell: DetailPostCell {
+    //MARK: - Properties
+    weak var delegate: ReportPopupOpenDelegate?
+    let profileImageView = UIImageView().then {
+        $0.image = Images.Detail.defaultProfile.image
+    }
+    let nicknameLabel = UILabel().then {
+        $0.font = Fonts.Pretendard.semiBold.font(size: 14)
+        $0.textAlignment = .left
+    }
+    let dateLabel = UILabel().then {
+        $0.font = Fonts.Pretendard.regular.font(size: 14)
+        $0.textColor = UIColor(colorSet: 117)
+        $0.textAlignment = .left
+    }
+    let moreButton = UIButton().then {
+        $0.setImage(Images.Detail.more.image, for: .normal)
+        $0.setImage(Images.Detail.more.image, for: .highlighted)
+    }
+    
+    //MARK: - Initializer
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        makeUI()
+        
+        moreButton.addTarget(self, action: #selector(moreButtonTouched), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Make UI
+    private func makeUI() {
+        selectionStyle = .none
+        
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(nicknameLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(moreButton)
+        
+        profileImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(14)
+            $0.left.equalToSuperview().offset(10)
+            $0.size.equalTo(46)
+        }
+        nicknameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(16)
+            $0.left.equalTo(profileImageView.snp.right)
+            $0.height.equalTo(22)
+        }
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(nicknameLabel.snp.bottom)
+            $0.left.equalTo(profileImageView.snp.right)
+            $0.height.equalTo(18)
+        }
+        moreButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.right.equalToSuperview()
+            $0.width.equalTo(56)
+        }
+    }
+    
+    func configure(withName name: String?, dateString: String?) {
+        guard let name else { return }
+        
+        nicknameLabel.text = name
+        dateLabel.text = DateManager.shared.timeString(target: dateString)
+    }
+    
+    @objc
+    private func moreButtonTouched() {
+        delegate?.openPopup()
+    }
+}
+
+//MARK: - Size 관련
+extension ProfileCell {
+    static var height: CGFloat = 72.0
+}
+
--- EightFront/Scenes/Clothes/Detail/DetailPostViewModel.swift
+++ EightFront/Scenes/Clothes/Detail/DetailPostViewModel.swift
@@ -6,23 +6,66 @@
 //
 
 import UIKit
+import Moya
 import Combine
-
 //MARK: DetailPostViewModel
 final class DetailPostViewModel {
     //MARK: - Properties
     var bag = Set<AnyCancellable>()
     let input = Input()
     let output = Output()
+    var apiCall = 0
+    let clothesProvider = MoyaProvider<ClothesAPI>()
+    var info: DetailPostModel? {
+        didSet {
+            output.requestPost.send(true)
+        }
+    }
+    var comments: [Comment]? {
+        didSet {
+            output.requestComments.send(true)
+        }
+    }
+    var id: Int?
+    
     
     //MARK: Initializer
-    init() {
+    init(id: Int?) {
+        self.id = id
         bind()
     }
     
     //MARK: RxBinding..
     private func bind() {
+        input.requestPost
+            .compactMap { $0 }
+            .sink { [weak self] identifier in
+                self?.requestPost(id: identifier)
+                self?.requestComments(id: identifier)
+            }
+            .store(in: &bag)
         
+        input.requestComments
+            .compactMap { $0 }
+            .sink { [weak self] identifier in
+                self?.requestComments(id: identifier)
+            }
+            .store(in: &bag)
+        
+        input.requestComment
+            .compactMap { $0 }
+            .sink { [weak self] comment in
+                self?.requestComment(text: comment)
+            }
+            .store(in: &bag)
+        
+        input.requestPostVote
+            .sink { [weak self] in
+                self?.requestKeepOrLock(with: $0)
+            }
+            .store(in: &bag)
+        
+        input.requestPost.send(id)
     }
 }
 
@@ -33,15 +76,115 @@ extension DetailPostViewModel {
     }
     
     struct Input {
-        
+        let requestPost = PassthroughSubject<Int?, Never>()
+        let requestComments = PassthroughSubject<Int?, Never>()
+        let requestPostVote = PassthroughSubject<String, Never>()
+        let requestComment = PassthroughSubject<String?, Never>()
     }
     
     struct Output {
-        
+        let requestPost = PassthroughSubject<Bool, Never>()
+        let requestComments = PassthroughSubject<Bool, Never>()
     }
 }
 
 //MARK: - Method
 extension DetailPostViewModel {
+    private func requestPost(id: Int) {
+        clothesProvider
+            .requestPublisher(.post(id: id))
+            .sink { [weak self] completion in
+                switch completion {
+                case .failure(let error):
+                    LogUtil.d(error.localizedDescription)
+                    
+                    guard (self?.apiCall ?? 3) < 3 else { return }
+                    self?.apiCall += 1
+                    self?.requestPost(id: id)
+                case .finished:
+                    LogUtil.d("Successed")
+                    self?.apiCall = 0
+                }
+            } receiveValue: { [weak self] response in
+                guard let responseData = try? response.map(PostResponse.self).data else { return }
+                self?.info = responseData.info
+            }
+            .store(in: &bag)
+    }
     
+    private func requestComments(id: Int) {
+        clothesProvider
+            .requestPublisher(.comments(id: id))
+            .sink { [weak self] completion in
+                switch completion {
+                case .failure(let error):
+                    LogUtil.d(error.localizedDescription)
+                    
+                    guard (self?.apiCall ?? 3) < 3 else { return }
+                    self?.apiCall += 1
+                    self?.requestPost(id: id)
+                case .finished:
+                    LogUtil.d("Successed")
+                    self?.apiCall = 0
+                }
+            } receiveValue: { [weak self] response in
+                guard let responseData = try? response.map(CommentsResponse.self).data else { return }
+                
+                var comments = [Comment]()
+                
+                for component in responseData.comments ?? [] {
+                    comments.append(Comment(type: 0,
+                                            nickname: component.nickname,
+                                            comment: component.comment,
+                                            createdAt: component.createdAt))
+                    comments.append(contentsOf: component.children ?? [])
+                }
+                
+                self?.comments = comments
+            }
+            .store(in: &bag)
+    }
+    
+    private func requestKeepOrLock(with vote: String) {
+        clothesProvider
+            .requestPublisher(.vote(type: VoteRequest(voteType: vote)))
+            .sink { [weak self] completion in
+                switch completion {
+                case .failure(let error):
+                    LogUtil.d(error.localizedDescription)
+                    
+                    guard (self?.apiCall ?? 3) < 3 else { return }
+                    self?.apiCall += 1
+                    self?.requestKeepOrLock(with: vote)
+                case .finished:
+                    LogUtil.d("Successed")
+                    self?.apiCall = 0
+                }
+            } receiveValue: { _ in }
+            .store(in: &bag)
+    }
+    
+    private func requestComment(text: String?) {
+        guard let id, let text else { return }
+        
+        let param = CommentRequest(comment: text)
+        
+        clothesProvider
+            .requestPublisher(.newComment(id: id, info: param))
+            .sink { [weak self] completion in
+                switch completion {
+                case .failure(let error):
+                    LogUtil.d(error.localizedDescription)
+                    
+                    guard (self?.apiCall ?? 3) < 3 else { return }
+                    self?.apiCall += 1
+                    self?.requestPost(id: id)
+                case .finished:
+                    LogUtil.d("Successed")
+                    self?.apiCall = 0
+                    self?.input.requestComments.send(self?.id)
+                }
+            } receiveValue: { _ in }
+            .store(in: &bag)
+    }
 }
