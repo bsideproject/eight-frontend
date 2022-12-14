6372e4eb8313550678022ca18684eef7ae9064eb
krwd1117 <krwd1117@gmail.com>
2022-12-14 23:06:45 +0900

    간편 회원가입 닉네임 확인 결과 라벨 표기

--- EightFront/Scenes/Auth/SignUp/SimpleSignUpVieModel.swift
+++ EightFront/Scenes/Auth/SignUp/SimpleSignUpVieModel.swift
@@ -16,95 +16,106 @@ class SimpleSignUpVieModel {
     
     @Published var signType: SignType?
 
-    @Published var inputNickname = ""
-    @Published var isSignUpButtonValid = false
+    @Published var inputNickname = "" {
+        didSet {
+            isNicknameCheck = false
+            isSignUpButtonValid = false
+            nicknameDuplicateText = ""
+        }
+    }
+    
+    @Published var nicknameDuplicateText = ""
     
     @Published var isNicknameCheck = false
+    @Published var isSignUpButtonValid = false
+    
     @Published var isSignUp = false
 
     lazy var isNicknameValid: AnyPublisher<Bool, Never> = $inputNickname
         .compactMap {
-            if $0.count > 1 {
-                // 2~16 글자
-                let nicknameRegEx = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]{1,15}"
-                let nicknameValid = NSPredicate(format:"SELF MATCHES %@", nicknameRegEx).evaluate(with: $0)
-                return nicknameValid
-            } else {
-                return false
-            }
+            print(self.inputNickname.count)
+            let nicknameRegEx = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]{2,16}"
+            let nicknameValid = NSPredicate(format:"SELF MATCHES %@", nicknameRegEx).evaluate(with: $0)
+            return nicknameValid
         }.eraseToAnyPublisher()
     
     func requestNickNameCheck() {
-        authProvider.requestPublisher(.nicknameCheck(nickname: inputNickname))
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    LogUtil.d("닉네임 변경 API 호출 완료")
-                case .failure(let error):
-                    LogUtil.e(error)
-                }
-            } receiveValue: { [weak self] response in
-                let data = try? response.map(NicknameCheckResponse.self)
-                if let content = data?.data?.content {
-                    if content {
-                        // 중복된 닉네임이 있을 때
-                        self?.isNicknameCheck = false
-                    } else {
-                        // 일치하는 닉네임이 없을 때
-                        self?.isNicknameCheck = true
+        if inputNickname.count > 1 {
+            authProvider.requestPublisher(.nicknameCheck(nickname: inputNickname))
+                .sink { completion in
+                    switch completion {
+                    case .finished:
+                        LogUtil.d("닉네임 변경 API 호출 완료")
+                    case .failure(let error):
+                        LogUtil.e(error)
                     }
-                }
-            }.store(in: &bag)
+                } receiveValue: { [weak self] response in
+                    let data = try? response.map(NicknameCheckResponse.self)
+                    if let content = data?.data?.content {
+                        if content {
+                            // 중복된 닉네임이 있을 때
+                            self?.isNicknameCheck = false
+                            self?.nicknameDuplicateText = "* 중복 된 닉네임입니다."
+                        } else {
+                            // 일치하는 닉네임이 없을 때
+                            self?.isNicknameCheck = true
+                            self?.nicknameDuplicateText = "* 사용 가능한 닉네임입니다."
+                        }
+                    }
+                }.store(in: &bag)
+        }
     }
     
     func requestSignUp() {
-        let accessToken = KeyChainManager.shared.accessToken
-        switch signType {
-        case .kakao:
-            LogUtil.d("카카오 회원가입")
-            authProvider.request(
-                .socialSignUp(
-                    param: SocialSignUpRequest(
-                        accessToken: accessToken,
-                        nickName: inputNickname
-                    ))) { [weak self] result in
-                        switch result {
-                        case .success(let response):
-                            LogUtil.d("회원가입 API 호출 성공")
-                            if let data = try? response.map(SimpleSignUpResponse.self).data {
-                                guard let accessToken = data.content?.accessToken else { return }
-                                if KeyChainManager.shared.create(accessToken, type: .accessToken) {
-                                    UserDefaults.standard.set(SignType.kakao.rawValue, forKey: "signType")
-                                    self?.isSignUp = true
+        if isNicknameCheck && isSignUpButtonValid {
+            let accessToken = KeyChainManager.shared.accessToken
+            switch signType {
+            case .kakao:
+                LogUtil.d("카카오 회원가입")
+                authProvider.request(
+                    .socialSignUp(
+                        param: SocialSignUpRequest(
+                            accessToken: accessToken,
+                            nickName: inputNickname
+                        ))) { [weak self] result in
+                            switch result {
+                            case .success(let response):
+                                LogUtil.d("회원가입 API 호출 성공")
+                                if let data = try? response.map(SimpleSignUpResponse.self).data {
+                                    guard let accessToken = data.content?.accessToken else { return }
+                                    if KeyChainManager.shared.create(accessToken, type: .accessToken) {
+                                        UserDefaults.standard.set(SignType.kakao.rawValue, forKey: "signType")
+                                        self?.isSignUp = true
+                                    }
                                 }
+                            case .failure(let error):
+                                LogUtil.e(error)
                             }
-                        case .failure(let error):
-                            LogUtil.e(error)
                         }
-                    }
-        case .apple:
-            authProvider.request(
-                .appleSignUp(
-                    param: SocialSignUpRequest(
-                        identityToken: accessToken,
-                        nickName: inputNickname
-                    ))) { [weak self] result in
-                        switch result {
-                        case .success(let response):
-                            LogUtil.d("회원가입 API 호출 성공")
-                            if let data = try? response.map(SimpleSignUpResponse.self).data {
-                                guard let accessToken = data.content?.accessToken else { return }
-                                if KeyChainManager.shared.create(accessToken, type: .accessToken) {
-                                    UserDefaults.standard.set(SignType.apple.rawValue, forKey: "signType")
-                                    self?.isSignUp = true
+            case .apple:
+                authProvider.request(
+                    .appleSignUp(
+                        param: SocialSignUpRequest(
+                            identityToken: accessToken,
+                            nickName: inputNickname
+                        ))) { [weak self] result in
+                            switch result {
+                            case .success(let response):
+                                LogUtil.d("회원가입 API 호출 성공")
+                                if let data = try? response.map(SimpleSignUpResponse.self).data {
+                                    guard let accessToken = data.content?.accessToken else { return }
+                                    if KeyChainManager.shared.create(accessToken, type: .accessToken) {
+                                        UserDefaults.standard.set(SignType.apple.rawValue, forKey: "signType")
+                                        self?.isSignUp = true
+                                    }
                                 }
+                            case .failure(let error):
+                                LogUtil.e(error)
                             }
-                        case .failure(let error):
-                            LogUtil.e(error)
                         }
-                    }
-        default:
-            LogUtil.e("오류")
+            default:
+                LogUtil.e("오류")
+            }
         }
     }
 }
